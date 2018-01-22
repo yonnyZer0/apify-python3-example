40f85f1fe2e6d8e5d3e1ec037e34693c759027be
yonnyZer0 <janzisko@gmail.com>
2018-01-22 21:19:42 +0100

    example added

--- example.py
+++ example.py
@@ -1,7 +1,7 @@
 #! /usr/bin/env python3
 
-#import urllib.request as u2
-#from lxml import html
+import urllib.request as u2
+from lxml import html
 import os
 
 print( str( os.environ ).replace(', ','\n') )
