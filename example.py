cf0a010dcb91331a4374269b3bbde47ddb7cad62
yonnyZer0 <janzisko@gmail.com>
2018-01-22 21:39:18 +0100

    example added

--- example.py
+++ example.py
@@ -4,7 +4,7 @@ import urllib.request as u2
 from lxml import html
 import os
 
-request = u2.Request('https://api.apify.com/v2/key-value-stores/' + os.environ['APIFY_DEFAULT_KEY_VALUE_STORE_ID'], headers={ 'Content-Type': 'application/json' })
+request = u2.Request('https://api.apify.com/v2/key-value-stores/' + os.environ['APIFY_DEFAULT_KEY_VALUE_STORE_ID'] + '/keys', headers={ 'Content-Type': 'application/json' })
 
 
 print( u2.urlopen(request).read() )
