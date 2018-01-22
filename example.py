e0ed369ae4103f6834d2d9172cdfa103debc1ea1
yonnyZer0 <janzisko@gmail.com>
2018-01-22 21:43:14 +0100

    example added

--- example.py
+++ example.py
@@ -7,6 +7,6 @@ import os, json
 request = u2.Request('https://api.apify.com/v2/key-value-stores/' + os.environ['APIFY_DEFAULT_KEY_VALUE_STORE_ID']+ '/records/INPUT', headers={ 'Content-Type': 'application/json' })
 
 
-print( json.loads( u2.urlopen(request).read() ) )
+print( json.loads( u2.urlopen(request).read().decode() ) )
 
 
